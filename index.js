0acb778cc5cba3ac2ff3367c6f862e31bd18a8b2
Isaac Proch <isaac@iproch.design>
2020-02-04 20:57:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const artists = ['XXXTentacion', 'Skizzy Mars', 'G-Eazy', 'Gambino'];
+
+artists.forEach(artist => {
+  console.log(artist + ' is one of the greatest artists.')
+});
+
+/*
+const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+artists.forEach(artist => {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squareNumbers = numbers.map(number => {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+const onlyNumbers = things.filter(thing => {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
+*/
